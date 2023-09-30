ccad8785c662fc1d47fa044805f195080372a03c
Remitito <jrossot@protonmail.com>
2023-09-30 16:36:36 +0100

    Removed other dead route

--- routes.js
+++ routes.js
@@ -3,9 +3,8 @@ const express = require('express');
 const router = express.Router();
 const QuizController = require('./controllers/QuizController.js');
 const Quiz = require('./models/Quiz.js');
-
 router.post('/uploadQuiz', QuizController.uploadQuiz)
-router.post('/logRequest', QuizController.logRequest)
+// router.post('/logRequest', QuizController.logRequest)
 router.get('/names/user', QuizController.getUserQuizNames)
 router.get('/quiz/user', QuizController.getUserQuiz)
 
